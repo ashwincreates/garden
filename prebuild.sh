ef2d3b0f07aebab9110d4b87d72b687e613501b4
ashwin-driscolls <Ashwin.Sharma@driscolls.com>
2024-04-05 19:48:04 +0530

    fix: build script

--- prebuild.sh
+++ prebuild.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 echo "Running Prebuild Script🌼"
+git submodule foreach git pull origin master
 mkdir ./public/images
 find ./content -type f -name '*.png' -exec cp -t ./public/images {} +
