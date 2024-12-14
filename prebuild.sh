c7cab97c3e3dfbe52e99536753adae8e4756d9ef
ashwincreates <ashwin200113@gmail.com>
2024-12-14 10:21:14 +0530

    fixed images

--- prebuild.sh
+++ prebuild.sh
@@ -2,5 +2,14 @@
 echo "Running Prebuild Script🌼"
 git submodule foreach git pull origin master
 node searchIndex.mjs
-# mkdir ./public/images
-# find ./content -type f -name '*.png' -exec cp -t ./public/images {} +
+# mkdir -p ./public/images
+# find ./content -type f -name '*.png' -exec cp ./public/images {} +
+
+# Target directory
+TARGET_DIR="./public/images"
+
+# Create the target directory if it doesn't exist
+mkdir -p "$TARGET_DIR"
+
+# Find and copy all .png, .jpeg, and .jpg files
+find . -type f \( -iname "*.png" -o -iname "*.jpeg" -o -iname "*.jpg" \) -exec cp {} "$TARGET_DIR" \;
